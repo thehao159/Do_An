f81afd21748e39beb1c6a8ef64a9464e2a613f29
thehao159 <44767198+thehao159@users.noreply.github.com>
2018-11-18 08:29:41 +0700

    Add files via upload

--- DoAn/DoAn_js.js
+++ DoAn/DoAn_js.js
@@ -1,9 +1,15 @@
 function opencertain()
 	{
   		document.getElementById("overlaycertainimg").style.display = "block";
+		
 	}
 
 function closecertain()
 	{
   		document.getElementById("overlaycertainimg").style.display = "none";
 	}
+
+function changepic(src)
+{
+	document.getElementById("bigimg").src = src;
+}
